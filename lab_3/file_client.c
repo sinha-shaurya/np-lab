64be258118117ab9465c526f470ab86d41dc0600
Shaurya Sinha <shaurya.sinha@protonmail.com>
2020-12-21 16:13:44 +0530

    removed junk

--- lab_3/file_client.c
+++ lab_3/file_client.c
@@ -11,43 +11,43 @@
 int main()
 {
 	int sockfd;
-    struct sockaddr_in servaddr;
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(PORT);
-    servaddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	struct sockaddr_in servaddr;
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(PORT);
+	servaddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    int n = connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	int n = connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
 
 	char s[MAX_SIZE];
-	memset(s,0,sizeof(s));
+	memset(s, 0, sizeof(s));
 	printf("Enter filename\n");
-	scanf("%s",s);
+	scanf("%s", s);
 	//send filename
-	int sent_bytes=send(sockfd,&s,strlen(s),0);
-	printf("%d",sent_bytes);
+	int sent_bytes = send(sockfd, &s, strlen(s), 0);
+	printf("%d", sent_bytes);
 	//receive file exists or not response
-	memset(s,0,sizeof(s));
-	recv(sockfd,&s,MAX_SIZE,0);
+	memset(s, 0, sizeof(s));
+	recv(sockfd, &s, MAX_SIZE, 0);
 	puts(s);
-	if(strcmp(s,"File does not exist")!=0)
+	if (strcmp(s, "File does not exist") != 0)
 	{
-		memset(s,0,sizeof(s));
-		recv(sockfd,&s,MAX_SIZE,0);
+		memset(s, 0, sizeof(s));
+		recv(sockfd, &s, MAX_SIZE, 0);
 		puts(s);
 		int choice;
 		printf("Enter choice\n");
-		scanf("%d",&choice);
-		send(sockfd,&choice,sizeof(int),0);
-		switch(choice)
+		scanf("%d", &choice);
+		send(sockfd, &choice, sizeof(int), 0);
+		switch (choice)
 		{
-			case 1:
-			memset(s,0,sizeof(s));
-			scanf("%s",s);
-			send(sockfd,&s,MAX_SIZE,0);
+		case 1:
+			memset(s, 0, sizeof(s));
+			scanf("%s", s);
+			send(sockfd, &s, MAX_SIZE, 0);
 		}
-		puts(s);	
+		puts(s);
 	}
 	close(sockfd);
 	return 0;
